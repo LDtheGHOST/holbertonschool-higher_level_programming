d79238b947548ee2c52da85210c1886edc0898ec
luis-david <luissmith@hotmail.fr>
2024-05-15 13:31:41 +0200

    chatgpt

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if last_digit > 5:
-    status = "and is greater than 5"
+status = "and is greater than 5"
 elif last_digit == 0:
-    status = "and is 0"
+status = "and is 0"
 else:
-    status = "and is less than 6 and not 0"
-    print(f"Last digit of {number} is {last_digit} {status}")
+status = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} {status}")
