7a0fe8ac6af905cf5dc5e808913a7d201af4bac5
luis-david <luissmith@hotmail.fr>
2024-05-15 13:57:36 +0200

    chatgpt

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
 for alphabet in range(ord('a'), ord('z') + 1):
-if chr(alphabet) not in ["q", "e"]:
-print("{:s}".format(chr(alphabet)), end="")
+    if chr(alphabet) not in ["q", "e"]:
+
+        print("{:s}".format(chr(alphabet)), end="")
