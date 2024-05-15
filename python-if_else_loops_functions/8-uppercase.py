ea20141e310594bf043da4e43440c56f57fadc7b
luis-david <luissmith@hotmail.fr>
2024-05-15 15:36:00 +0200

    chatgpt

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        char = ord(str[i])
-        
-        if 97 <= char <= 122:
-            char = char - 32
-            print("{:c}".format(char), end="")
-            print()
+        s = str[i]
+        unicode = ord(s)
+        if unicode >= 97 and unicode <= 122:
+            s = chr(unicode - 32)
+        print("{:s}".format(s), end="")
+    print("")
