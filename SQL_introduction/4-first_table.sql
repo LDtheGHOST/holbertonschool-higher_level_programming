b11a80c258ecfe747c217d9f035afbdc6be7a1e3
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-28 17:03:38 +0300

    SQL - Introduction

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- ( CREATE TABLE ) va cree la table plus que le nom de la table --
-SQL CREATE TABLE IF EXISTS first_table
+SQL CREATE TABLE IF NOT EXISTS first_table
 (
     id INT
     name VACHAR(256)
