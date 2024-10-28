425880e6f88660eb507c7d71c32160190b358dbb
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-28 15:35:19 +0300

    sandere

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- ( CREATE DATABASES IF NOT EXISTE ) avec le nom du ficher a la fin pour voir si il est bien la -- 
-CREATE DATABASES IF NOT EXISTE hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTE hbtn_0c_0;
