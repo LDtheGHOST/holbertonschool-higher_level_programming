a864f460da88470fdea547fd816bc217aabc1d09
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-28 15:36:29 +0300

    sandere

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,2 +1,2 @@
 -- (SHOW) liste toute les batase de donnée --
-SHOW DATABASES 
+SHOW DATABASE
--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- ( CREATE DATABASES IF NOT EXISTE ) avec le nom du ficher a la fin pour voir si il est bien la -- 
-CREATE DATABASE IF NOT EXISTE hbtn_0c_0;
+-- ( CREATE DATABASES IF NOT EXISTS ) avec le nom du ficher a la fin pour voir si il est bien la -- 
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
