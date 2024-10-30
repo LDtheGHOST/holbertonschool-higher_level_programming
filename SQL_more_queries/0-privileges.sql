592693221ab316dc7dca774ddfb13533e659419b
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-30 15:45:41 +0300

    SQL - Introduction

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- C'est quoi Grant SQL ? Permet d'accorder des autorisations sur une table, une vue, une fonction table, une procédure stockée, une procédure stockée étendue, une fonction scalaire, une fonction d'agrégation, une file d'attente de service ou un synonyme. --
-SHOW GRANTS FOR "user_0d_1" @ "localhost"
-SHOW GRANTS FOR "user_0d_2" @ "localhost"
+SHOW GRANTS FOR "user_0d_1" @ "localhost";
+SHOW GRANTS FOR "user_0d_2" @ "localhost";
