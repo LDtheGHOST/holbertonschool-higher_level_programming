c1d9ffab4791f0aa94138e9919a728cc859b9073
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-30 16:31:04 +0300

    SQL - Introduction

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- un script cree l'utilisatueur et donne des privileges --
 CREATE USER IF NOT EXIST "user_0d_1" @ "localhost" IDENTIFIED BY user_0d_1_pwd:
+-- donne les privilege --
 GRANT ALL PRIVILEGES ON *,* TO "user_0d_1" @ "locahost" WITH GRANT OPTION
